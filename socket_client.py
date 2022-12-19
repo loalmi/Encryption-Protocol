6c24a0811bddb2c8ecd34c03653385db9d98a682
loalmi <114482797+loalmi@users.noreply.github.com>
2022-12-19 03:45:51 +0300

    Add files via upload

--- socket_client.py
+++ socket_client.py
@@ -1,8 +1,34 @@
 import socket
+import pickle
+from crypt_utils import DiffieHellman, FileCrypter
 s = socket.socket()
 
 s.connect(('localhost', 3333)) # Подключаемся к нашему серверу.
 s.sendall('Hello! GOOOOD_NIGHTTTT'.encode('utf-8')) # Отправляем фразу.
 data = s.recv(1024) #Получаем данные из сокета.
+
+p = 54
+g = 53
+a = 63  # это можно хранить в txt
+
+diffie_hellman = DiffieHellman(a=a, p=p, g=g)
+client_mixed_key = diffie_hellman.mixed_key
+private_key = diffie_hellman.generate_key(client_mixed_key)
+print(client_mixed_key)
+print(private_key)
+
+s.send(pickle.dumps((p, g, client_mixed_key)))
+s.close()
+
+s = socket.socket()
+s.connect(('localhost', 3333))
+crypter = FileCrypter(private_key)
+result = crypter.encryption("ТЕСТОВАЯ СТРОКАААА")
+print(result)
+s.send(pickle.dumps(result))
+
+result = crypter.encryption(result)
+print(result)
+
 s.close()
 print(data)
--- socket_server.py
+++ socket_server.py
@@ -1,14 +1,29 @@
-
+from crypt_utils import DiffieHellman, FileCrypter
 import socket
 sock = socket.socket()
 sock.bind(('', 3333)) # Привязываем серверный сокет к localhost и 3030 порту.
 sock.listen(1) # Начинаем прослушивать входящие соединения
 conn, addr = sock.accept() # Метод который принимает входящее соединение.
 
+crypter = None
 while True: # Создаем вечный цикл.
 	data = conn.recv(1024) # Получаем данные из сокета.
-	if not data:
-		break
+	if type(data) == tuple:
+		p, g, A = data
+
+		diffie_hellman = DiffieHellman(a=A, p=p, g=g)
+		server_mixed_key = diffie_hellman.mixed_key
+		private_key = diffie_hellman.generate_key(server_mixed_key)
+		crypter = FileCrypter(private_key)
+		print(server_mixed_key)
+		print(private_key)
+
+	elif type(data) == str:
+		result = crypter.encryption(data)
+		print(result)
+
+	else:
+		raise ValueError(f"Был принят некорректный тип data: {type(data)}")
 	conn.sendall(data) # Отправляем данные в сокет.
 	print(data.decode('utf-8')) # Выводим информацию на печать.
 conn.close()
